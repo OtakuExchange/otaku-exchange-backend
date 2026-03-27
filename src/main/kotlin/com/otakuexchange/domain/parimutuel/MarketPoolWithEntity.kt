0bb771e3ac1aecd7001b9b48c88aa2fa879bbe67
cjdoherty1 <cjdoherty883@gmail.com>
2026-03-26 22:24:11 -0400

    added some shit

---
+++ src/main/kotlin/com/otakuexchange/domain/parimutuel/MarketPoolWithEntity.kt
@@ -0,0 +1,20 @@
+package com.otakuexchange.domain.parimutuel
+
+import com.otakuexchange.domain.market.Entity
+import kotlinx.serialization.Serializable
+import kotlin.time.Clock
+import kotlin.time.Instant
+import kotlin.uuid.Uuid
+
+@Serializable
+data class MarketPoolWithEntity(
+    val id: Uuid = Uuid.random(),
+    val eventId: Uuid,
+    val entity: Entity? = null,
+    val label: String,
+    val isWinner: Boolean = false,
+    val amount: Int = 0,
+    val volume: Long = 0L,
+    val createdAt: Instant = Clock.System.now(),
+    val updatedAt: Instant = Clock.System.now()
+)
